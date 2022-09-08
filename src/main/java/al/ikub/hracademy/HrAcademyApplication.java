3fe46d70da6a746c042f79fd8dc97bd43013e992
fdodaj <floriandodaj55@gmail.com>
2022-09-08 12:19:10 +0200

    init

---
+++ src/main/java/al/ikub/hracademy/HrAcademyApplication.java
@@ -0,0 +1,13 @@
+package al.ikub.hracademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/ikub/hracademy/HrAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package al.ikub.hracademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
