15228fbedac21fb537c8ae3675496bba44409093
fdodaj <floriandodaj55@gmail.com>
2022-09-24 20:50:59 +0200

    fix profiles + apply postgres

--- src/main/resources/db/migration/V1.1__ddl_create_student.sql
+++ src/main/resources/db/migration/V1.1__ddl_create_student.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS `student` (
-    `id`         INTEGER  PRIMARY KEY AUTO_INCREMENT,
-    `first_name` VARCHAR(50) NOT NULL,
-    `last_name`  VARCHAR(50)  NOT NULL,
-    `phone_number`  VARCHAR(50)  NOT NULL,
-    `email`      VARCHAR(50)  NOT NULL,
-    `status`     VARCHAR(50),
-    `reference`  VARCHAR(50),
-    `price_reduction`  DOUBLE,
-    `price_paid` DOUBLE,
-    `comment`    VARCHAR(100),
-    `date_added` DATETIME not null ,
-    `last_modified`    DATETIME not null ,
-    `deleted`    BOOLEAN not null ,
-    `student_course`    INT not null
+CREATE TABLE IF NOT EXISTS student (
+   id       INTEGER  PRIMARY KEY,
+   first_name VARCHAR(50) NOT NULL,
+   last_name  VARCHAR(50)  NOT NULL,
+   phone_number  VARCHAR(50)  NOT NULL,
+   email    VARCHAR(50)  NOT NULL,
+   status    VARCHAR(50),
+   reference  VARCHAR(50),
+   price_reduction  numeric,
+   price_paid numeric,
+   comment    VARCHAR(100),
+   date_added timestamp not null ,
+   last_modified    timestamp not null ,
+   deleted    BOOLEAN not null ,
+   student_course integer not null
 );
--- src/main/resources/db/migration/V1.2__ddl_create_course.sql
+++ src/main/resources/db/migration/V1.2__ddl_create_course.sql
@@ -1,11 +1,11 @@
 
-CREATE TABLE IF NOT EXISTS `course` (
-    `id`             INTEGER  PRIMARY KEY AUTO_INCREMENT,
-    `name`           VARCHAR(50) NOT NULL,
-    `status`         VARCHAR(50),
-    `start_date`     DATETIME not null ,
-    `end_date`       DATETIME not null ,
-    `date_added`     DATETIME not null ,
-    `last_modified`  DATETIME not null ,
-    `deleted`        BOOLEAN not null
+CREATE TABLE IF NOT EXISTS course (
+    id             INTEGER  PRIMARY KEY,
+    name           VARCHAR(50) NOT NULL,
+    status         VARCHAR(50),
+    start_date     timestamp not null ,
+    end_date       timestamp not null ,
+    date_added     timestamp not null ,
+    last_modified  timestamp not null ,
+    deleted        BOOLEAN not null
 );
